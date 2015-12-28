e65a4a0bec743084c1efdbb34d3fdbb4dc8d8fbd
Itzik Ephraim <oranja@gmail.com>
2015-12-28 09:55:56 +0200

    Change emit event injection code for BSD/OSX sed compatibility

--- init.fish
+++ init.fish
@@ -3,9 +3,13 @@ function init --on-event init_sudope
   # because Fish overrides bindings defined in config.fish,
   # and therefore everything that OMF's init tries to define.
 
-  if not test (functions fish_user_key_bindings | grep "emit omf_bind_keys")
-    funced fish_user_key_bindings -e "sed -i'' '/^end\$/i \
-      emit omf_bind_keys'"
+  set -l emit_bind_keys "emit omf_bind_keys"
+  set -l fish_key_binding_fn "fish_user_key_bindings"
+
+  if not functions -q $fish_key_binding_fn
+    eval "function $fish_key_binding_fn; $emit_bind_keys; end"
+  else if not test (functions $fish_key_binding_fn | grep $emit_bind_keys)
+    functions $fish_key_binding_fn | sed "\$i \  $emit_bind_keys" | source
   end
 
   source $OMF_PATH/pkg/sudope/functions/sudope.init.bind_keys.fish
