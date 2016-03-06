6ee94501b7b300247af7c384786865ba5b4cc4a8
Itzik Ephraim <oranja@gmail.com>
2016-03-06 22:43:05 +0200

    Prepare sudope for the new OMF init method

--- functions/sudope.fish
+++ functions/sudope.fish
@@ -6,7 +6,7 @@ function sudope -d "Quickly toggle sudo prefix"
   # Read the current command line, tokenize it, store it in $cmd_buffer.
   commandline | read -la cmd_buffer
   # Either put "sudo" or remove it from the beginning of the line.
-  if not contains "sudo" $cmd_buffer
+  if not contains -- "sudo" $cmd_buffer
     commandline -r "sudo $cmd_buffer"
   else
     commandline -r (echo "$cmd_buffer" | sed -E s/"^sudo\s+(.*)"/"\1"/)
--- init.fish
+++ init.fish
@@ -1,16 +1,1 @@
-function init --on-event init_sudope
-  # Here be dragons and code injection,
-  # because Fish overrides bindings defined in config.fish,
-  # and therefore everything that OMF's init tries to define.
-
-  set -l emit_bind_keys "emit omf_bind_keys"
-  set -l fish_key_binding_fn "fish_user_key_bindings"
-
-  if not functions -q $fish_key_binding_fn
-    eval "function $fish_key_binding_fn; $emit_bind_keys; end"
-  else if not test (functions $fish_key_binding_fn | grep $emit_bind_keys)
-    functions $fish_key_binding_fn | sed "\$i \  $emit_bind_keys" | source
-  end
-
-  source $OMF_PATH/pkg/sudope/functions/sudope.init.bind_keys.fish
-end
+# Nothing to do here. See key_bindings.fish
