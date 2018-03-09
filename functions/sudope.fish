4edc52e9b121fee38c350086381a1057d2c88b66
Itzik Ephraim <oranja@gmail.com>
2018-03-09 14:21:33 +0200

    Handle multi-line commands
    Whenever the command line spans more than one line, the first line will
    receive or lose the `sudo` command and the rest are left unaffected.

--- functions/sudope.fish
+++ functions/sudope.fish
@@ -5,22 +5,27 @@ function sudope -d "Quickly toggle sudo prefix"
   set -l cursor_position (commandline -C)
 
   # If the command line is empty, pull the last command from history.
-  if test -z $command_buffer
+  if test -z "$command_buffer"
     set command_buffer $history[1]
   end
 
-  # Parse the command line.
-  set -l command_parts (string match -ir '^(\s*)(sudo(\s+|$))?(.*)' $command_buffer)
+  # Parse the command line (first line only).
+  set -l command_parts (string match -ir '^(\s*)(sudo(\s+|$))?(.*)' $command_buffer[1])
+
+  # Handle multiline commands with extra care.
+  set -l command_lines_count (count $command_buffer)
+  test $command_lines_count -gt 1
+    and set -l command_rest $command_buffer[2..$command_lines_count]
 
   switch (count $command_parts)
     case 3
       # No "sudo".
 
       # Add "sudo" to the beginning of the command, after any leading whitespace (if present).
-      commandline -r (string join '' $command_parts[2] 'sudo ' $command_parts[3])
+      commandline -r (string join \n (string join '' $command_parts[2] 'sudo ' $command_parts[3]) $command_rest)
 
       # Push the cursor position ahead if necessary (+5 for 'sudo ').
-      test $cursor_position -ge (string length $command_parts[2])
+      test $cursor_position -ge (string length -- "$command_parts[2]")
         and set cursor_position (math $cursor_position+5)
 
       # Place the cursor where it was (or where it should be).
@@ -30,11 +35,11 @@ function sudope -d "Quickly toggle sudo prefix"
       # "sudo" is present in the beginning of the command.
 
       # Remove "sudo", leave any leading whitespace (if present).
-      commandline -r (string join '' $command_parts[2 5])
+      commandline -r (string join \n (string join '' $command_parts[2 5]) $command_rest)
 
       # Push the cursor position back if appropriate ('sudo' and whitespace).
-      set -l lead_length (string length $command_parts[2])
-      set -l sudo_length (string length $command_parts[3])
+      set -l lead_length (string length -- "$command_parts[2]")
+      set -l sudo_length (string length -- "$command_parts[3]")
       if test $cursor_position -ge (math $lead_length+$sudo_length)
         # The cursor was after "sudo".
         set cursor_position (math $cursor_position-$sudo_length)
