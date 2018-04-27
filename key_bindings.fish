3f79cd86af0bc87b83b119e6dda147e079ad4ed6
Bjorn Neergaard <bjorn@neersighted.com>
2018-04-27 16:44:53 -0600

    Fix keybinding logic
    Fix the existing binding showing a warning, and remove a legacy redirection

--- key_bindings.fish
+++ key_bindings.fish
@@ -3,12 +3,11 @@ set -q sudope_sequence
   or set -l sudope_sequence \cs
 
 # if sudope is already bound to some sequence, leave it
-test (bind | grep -q '[[:space:]]sudope$')
-  or set -l do_bind
-
-# not bound but sequence already taken?
-if test (bind $sudope_sequence ^/dev/null)
-  echo "sudope: The requested sequence is already in use:" (bind $sudope_sequence | cut -d' ' -f2-)
-else if set -q do_bind
-  bind $sudope_sequence sudope
+if not test (bind | grep -q '[[:space:]]sudope$')
+  # not bound but sequence already taken?
+  if test (bind $sudope_sequence 2>/dev/null)
+    echo "sudope: The requested sequence is already in use:" (bind $sudope_sequence | cut -d' ' -f2-)
+  else
+    bind $sudope_sequence sudope
+  end
 end
