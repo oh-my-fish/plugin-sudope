b4849088465e09dcdf8092b12122ad84e6b325ba
Bjorn Neergaard <bjorn@neersighted.com>
2018-05-01 12:07:57 -0600

    Fix key binding logic (redux)
    The use of `test ()` here was incorrect and pointless

--- key_bindings.fish
+++ key_bindings.fish
@@ -3,9 +3,9 @@ set -q sudope_sequence
   or set -l sudope_sequence \cs
 
 # if sudope is already bound to some sequence, leave it
-if not test (bind | grep -q '[[:space:]]sudope$')
+if not bind | string match -rq '[[:space:]]sudope$'
   # not bound but sequence already taken?
-  if test (bind $sudope_sequence 2>/dev/null)
+  if bind $sudope_sequence >/dev/null 2>/dev/null
     echo "sudope: The requested sequence is already in use:" (bind $sudope_sequence | cut -d' ' -f2-)
   else
     bind $sudope_sequence sudope
