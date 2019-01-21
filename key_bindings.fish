a61942d7cde895c785e66d5a8ad31c267233596d
Barbz <BarbzYHOOL@users.noreply.github.com>
2019-01-21 22:08:01 +0100

    Use alt+s instead of ctrl+s (#9)
    * Use alt+s instead of ctrl+s
    
    Because in fish 3.0, ctrl+s is taken
    
    -- Much needed fix submitted by @BarbzYHOOL.

--- key_bindings.fish
+++ key_bindings.fish
@@ -1,6 +1,6 @@
-# default key sequence: Ctrl+s
+# default key sequence: Alt+s
 set -q sudope_sequence
-  or set -l sudope_sequence \cs
+  or set -l sudope_sequence \es
 
 # if sudope is already bound to some sequence, leave it
 if not bind | string match -rq '[[:space:]]sudope$'
