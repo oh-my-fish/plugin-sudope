936037ea25605b847f0b68f0394bea9a241d7b06
Matteo Savatteri <matteo.savatteri@bruttocarattere.org>
2020-09-15 16:49:12 +0200

    Add custom super user command

--- functions/sudope.fish
+++ functions/sudope.fish
@@ -1,8 +1,16 @@
-function sudope -d "Quickly toggle sudo prefix"
+function sudope -d "Quickly toggle super user command prefix"
 
   # Save the current command line and cursor position.
   set -l command_buffer (commandline)
   set -l cursor_position (commandline -C)
+
+  # If no custom super user command defaults to sudo
+  if test -z "$su_command"
+    set su_command 'sudo'
+  end
+
+  # Save su command length
+  set -l su_command_length (string length -- "$su_command")
 
   # If the command line is empty, pull the last command from history.
   if test -z "$command_buffer"
@@ -10,7 +18,7 @@ function sudope -d "Quickly toggle sudo prefix"
   end
 
   # Parse the command line (first line only).
-  set -l command_parts (string match -ir '^(\s*)(sudo(\s+|$))?(.*)' $command_buffer[1])
+  set -l command_parts (string match -ir "^(\s*)($su_command(\s+|\$))?(.*)" $command_buffer[1])
 
   # Handle multiline commands with extra care.
   set -l command_lines_count (count $command_buffer)
@@ -19,32 +27,32 @@ function sudope -d "Quickly toggle sudo prefix"
 
   switch (count $command_parts)
     case 3
-      # No "sudo".
+      # No su command.
 
-      # Add "sudo" to the beginning of the command, after any leading whitespace (if present).
-      commandline -r (string join \n (string join '' $command_parts[2] 'sudo ' $command_parts[3]) $command_rest)
+      # Add the su command to the beginning of the command, after any leading whitespace (if present).
+      commandline -r (string join \n (string join '' $command_parts[2] "$su_command " $command_parts[3]) $command_rest)
 
-      # Push the cursor position ahead if necessary (+5 for 'sudo ').
+      # Push the cursor position ahead if necessary
       test $cursor_position -ge (string length -- "$command_parts[2]")
-        and set cursor_position (math $cursor_position+5)
+        and set cursor_position (math $cursor_position+$su_command_length)
 
       # Place the cursor where it was (or where it should be).
       commandline -C $cursor_position
 
     case 5
-      # "sudo" is present in the beginning of the command.
+      # The su comamnd is present in the beginning of the command.
 
-      # Remove "sudo", leave any leading whitespace (if present).
+      # Remove the su command, leave any leading whitespace (if present).
       commandline -r (string join \n (string join '' $command_parts[2 5]) $command_rest)
 
-      # Push the cursor position back if appropriate ('sudo' and whitespace).
+      # Push the cursor position back if appropriate (su command and whitespace).
       set -l lead_length (string length -- "$command_parts[2]")
-      set -l sudo_length (string length -- "$command_parts[3]")
-      if test $cursor_position -ge (math $lead_length+$sudo_length)
-        # The cursor was after "sudo".
-        set cursor_position (math $cursor_position-$sudo_length)
+      set su_command_length (string length -- "$command_parts[3]")
+      if test $cursor_position -ge (math $lead_length+$su_command_length)
+        # The cursor was after the su command.
+        set cursor_position (math $cursor_position-$su_command_length)
       else if test $cursor_position -ge $lead_length
-        # The cursor was somewhere on "sudo".
+        # The cursor was somewhere on the su command.
         set cursor_position $lead_length
       end
 
