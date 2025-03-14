82c45291eb5d93e17b6a7037b6d73a38d382b1ed
X <50459973+ly4096x@users.noreply.github.com>
2025-03-14 08:18:59 -0700

    update escape sequence

--- conf.d/plugin-sudope.fish
+++ conf.d/plugin-sudope.fish
@@ -6,7 +6,7 @@ set -q sudope_sequence
       set sudope_sequence \cs
     case '*'
       # use Esc+Esc for fish 2.3+
-      set sudope_sequence \e\e
+      set sudope_sequence "escape,escape"
   end
 
 function __sudope_bind -a sequence # modifiers..
